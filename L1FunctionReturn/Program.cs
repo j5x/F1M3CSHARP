96fce948994a3bc5df88cb7f1e1901347a030e34
j5x <32881@ma-web.nl>
2022-03-28 11:29:56 +0200

    cgs

---
+++ L1FunctionReturn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace L1FunctionReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine(Name("Jason", "Siegersma")); }
+
+        static string Name(string firstName, string lastName)
+        {
+            string separator = ",";
+            string result = lastName + separator + firstName;
+            return result;
+        }
+
+    }
+}
---
+++ TimingL1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace get_current_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string datetime = DateTime.Now.ToString(" dddddd:dd:hhhh:mmmm:ss:yyyy tt");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(datetime);
+
+            }
+           
+
+        }
+    }
+}
